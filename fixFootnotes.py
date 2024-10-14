b02600418999d26ef007c0447934527df9715f0d
Jasper Wong <wlcjasper@gmail.com>
2024-10-14 01:55:30 +0100

    Fix footnotes

--- fixFootnotes.py
+++ fixFootnotes.py
@@ -1,13 +1,14 @@
 from bs4 import BeautifulSoup
 import os
+import re
 
 # Function to update href attributes in an HTML file
 def update_href(file_path):
     with open(file_path, 'r+', encoding='utf-8') as file:
         html_content = file.read()
         soup = BeautifulSoup(html_content, 'html.parser')
-        for link in soup.find_all('a', href="#Fn5"):
-            link['href'] = "./section9/#Fn5"
+        for link in soup.find_all('a', href=re.compile(r'^#Fn\d+$')):
+            link['href'] = "./section_9.html" + link['href']
         file.seek(0)
         file.write(str(soup))
         file.truncate()
